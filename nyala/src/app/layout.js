f010430903e6d2441224bb9c8308aa11e6f371cb
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-13 09:26:57 +0100

    meer aanpassingen

--- nyala/src/app/layout.js
+++ nyala/src/app/layout.js
@@ -1,6 +1,9 @@
+import headerComponent from "@/components/headerComponent";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <headerComponent />
       <body>{children}</body>
     </html>
   );
