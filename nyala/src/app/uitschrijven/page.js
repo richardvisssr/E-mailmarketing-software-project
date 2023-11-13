d6884c9be8eef705ac67bec48391a8193e3ff4dc
Matthias Budding <MD.Budding@student.han.nl>
2023-11-13 10:12:47 +0100

    start gemaakt

---
+++ nyala/src/app/uitschrijven/page.js
@@ -0,0 +1,10 @@
+import UitschrijfForm from "@/components/uitschrijfForm.js";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Uitschrijven</h1>
+      <UitschrijfForm />
+    </div>
+  );
+}
---
+++ nyala/src/components/uitschrijfForm.js
@@ -0,0 +1,16 @@
+export default function UitschrijfForm() {
+  return (
+    <div>
+      <form>
+        <label>
+          Input 1: <input type="text" />
+        </label>
+
+        <label>
+          Input 2: <input type="text" />
+        </label>
+        <button type="submit">Verstuur</button>
+      </form>
+    </div>
+  );
+}
