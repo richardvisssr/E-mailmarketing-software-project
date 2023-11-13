1c3c221e035790a264723ca985ab56a0c5c9393b
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-13 12:45:29 +0100

    formpie

--- nyala/src/components/ToevoegVeld.jsx
+++ nyala/src/components/ToevoegVeld.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import styles from "../app/handmatigToevoegen/toevoeg.module.css";
 
 export default function ToevoegVeld(props) {
-  const [email, setEmail] = useState("");
+  const [data, setData] = useState({ email: "", lijst: "" });
   const [foutmelding, setFoutmelding] = useState("");
 
   useEffect(() => {
-    if (email !== "") {
+    if (data.email !== "") {
       const postEmail = async () => {
         const response = fetch("", {
           method: "POST",
@@ -25,22 +25,33 @@ export default function ToevoegVeld(props) {
       };
       // postEmail();
     }
-  }, [setEmail]);
+  }, [data]);
 
   return (
     <div className="d-flex justify-content-center align-items-center">
       <div>
-        <div className={`input-group ${styles.vorm}`}>
-          <label htmlFor="vorm" className={`${styles.label} mb-1`}>
-            Vul een email in, om toe te voegen aan een emaillijst*
+        <div className={`input-group ${styles.vorm} d-flex flex-column`}>
+          <label htmlFor="vorm" className={`${styles.label} mb-2 rounded`}>
+            Vul een email in, om toe te voegen aan een emaillijst
           </label>
-          <input
-            type="text"
-            className={`form-control ${styles.invoer}`}
-            placeholder="Email"
-            aria-describedby="basic-addon1"
-            id="vorm"
-          />
+          <div id="vorm">
+            <input
+              type="text"
+              className={`form-control ${styles.invoer} p-2 mb-3`}
+              placeholder="Email"
+              aria-describedby="basic-addon1"
+            />
+
+            <input
+              type="text"
+              className={`form-control ${styles.invoer} p-2 mb-2 rounded`}
+              placeholder="Lijst"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <button type="button" className={`btn ${styles.knop} rounded`}>
+            Primary
+          </button>
         </div>
       </div>
     </div>
