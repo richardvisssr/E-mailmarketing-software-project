36a1a4cb27dac4b77c5908ee3c1bc66bebf69f8a
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-14 10:09:18 +0100

    Header werkt
    Alleen de Links moeten nog de echte locatie krijgen, wanneer de pagina's bestaan

--- nyala/src/components/headerComponent.js
+++ nyala/src/components/headerComponent.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./HeaderComponent.module.css";
 
 export default function HeaderComponent(props) {
   return (
     <header
-      className={`d-flex flex-wrap justify-content-around align-items-center py-1 mb-4 ml-2 border-bottom`}
-      style={{ backgroundColor: "black" }}
+      className={`d-flex flex-wrap justify-content-around align-items-center py-1 mb-4 ml-2 border-bottom ${styles.darkHeader}`}
     >
       <a
         target="_blank"
@@ -18,24 +18,37 @@ export default function HeaderComponent(props) {
           width={75}
           height={75}
           quality={100}
+          className={`${styles.logo}`}
         />
       </a>
 
       <ul className="nav nav-pills">
         <li className="nav-item">
-          <Link href="/" className="nav-link" aria-current="page">
+          <Link
+            href="/"
+            className={`${styles.linky} nav-link`}
+            aria-current="page"
+          >
             Templates
           </Link>
         </li>
         <span className="fs-4 text-dark">|</span>
         <li className="nav-item">
-          <Link href="/" className="nav-link" aria-current="page">
+          <Link
+            href="/"
+            className={`${styles.linky} nav-link`}
+            aria-current="page"
+          >
             Lid toevoegen
           </Link>
         </li>
         <span className="fs-4 text-dark">|</span>
         <li className="nav-item">
-          <Link href="/" className="nav-link" aria-current="page">
+          <Link
+            href="/"
+            className={`${styles.linky} nav-link`}
+            aria-current="page"
+          >
             Analyse
           </Link>
         </li>
