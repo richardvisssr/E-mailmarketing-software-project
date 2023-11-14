00b6f19871b519b5e528ce4902ea3ceabb6bc6ad
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-14 15:28:46 +0100

    ben bezig

--- nyala/src/app/handmatigToevoegen/page.js
+++ nyala/src/app/handmatigToevoegen/page.js
@@ -1,7 +1,7 @@
 "use client"
 
 import styles from './toevoeg.module.css'
-import ToevoegVeld from "@/components/toevoegVeld";
+import ToevoegVeld from "@/components/ToevoegVeld";
 
 export default function page({ params }) {
   return <ToevoegVeld />;
