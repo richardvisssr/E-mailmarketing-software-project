2ff761d9144f73a57213bfa08499b7aa8d2d1046
Matthias Budding <MD.Budding@student.han.nl>
2023-11-14 15:21:50 +0100

    Werkende basis

---
+++ nyala/src/app/uitgeschreven/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div
+      className="alert alert-success mt-3 d-flex justify-content-center"
+      role="alert"
+    >
+      U bent succesvol uitgeschreven
+    </div>
+  );
+}
--- nyala/src/components/uitschrijfForm.js
+++ nyala/src/components/uitschrijfForm.js
@@ -1,8 +1,16 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./uitschrijfForm.module.css";
+
 export default function UitschrijfForm({}) {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [reden, setReden] = useState("");
+  const [customReason, setCustomReason] = useState("");
+  const [warning, setWarning] = useState(null);
+
+  const reasons = ["Te veel mails", "Ik heb geen interesse meer", "Anders"];
 
   const handleUnsubscribe = async () => {
     try {
@@ -19,9 +27,15 @@ export default function UitschrijfForm({}) {
 
       if (unsubscribeResponse.status === 200) {
         console.log("Subscriber removed");
+        setWarning(null);
         return true;
       } else if (unsubscribeResponse.status === 404) {
         console.log("Subscriber not found");
+        setWarning(
+          <div className="alert alert-danger mt-3" role="alert">
+            Vul een geldig email adres in
+          </div>
+        );
         return false;
       } else {
         console.log("Failed to unsubscribe");
@@ -59,9 +73,16 @@ export default function UitschrijfForm({}) {
     setEmail(e.target.value);
   };
 
-  const changeReason = (e) => {
-    e.preventDefault();
-    setReden(e.target.value);
+  const changeReason = (selectedReason) => {
+    setReden(selectedReason);
+
+    if (selectedReason === "Anders") {
+      setCustomReason("");
+    }
+  };
+
+  const handleCustomReasonChange = (e) => {
+    setCustomReason(e.target.value);
   };
 
   const handleSubmit = async (e) => {
@@ -70,33 +91,64 @@ export default function UitschrijfForm({}) {
     console.log(sub);
     if (sub) {
       const reason = await handleReasonSubmit();
-      console.log("iehogbeiong");
       if (reason) {
         console.log("Reason added");
+        router.push("../uitgeschreven");
       }
     }
   };
 
   return (
     <div className="d-flex align-items-center flex-column">
-      <h1 className="mb-4">Uitschrijven</h1>
+      <h1 className="mb-4 mt-3">Uitschrijven</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label className="form-label">Email address</label>
-          <input type="email" className="form-control" onChange={changeEmail} />
+          <label className="form-label">Email adres</label>
+          <input
+            type="email"
+            className="form-control w-150"
+            onChange={changeEmail}
+          />
         </div>
         <div className="mb-3">
-          <label className="form-label">Geef hier uw reden op</label>
-          <textarea
-            className="form-control"
-            rows="3"
-            onChange={changeReason}
-          ></textarea>
+          <label className="form-label">Selecteer uw reden</label>
+          <ul>
+            {reasons.map((reason, index) => (
+              <li
+                key={index}
+                className={`${styles.customList} d-flex align-items-center mb-2`}
+              >
+                <label className="d-flex align-items-center">
+                  <input
+                    type="radio"
+                    name="unsubscribeReason"
+                    value={reason}
+                    onChange={() => changeReason(reason)}
+                    className="me-2 form-check-input"
+                  />
+                  {reason === "Anders" && (
+                    <div className="d-flex align-items-center">
+                      <label className="me-2">Anders:</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Typ hier uw reden"
+                        value={customReason}
+                        onChange={handleCustomReasonChange}
+                      />
+                    </div>
+                  )}
+                  {reason !== "Anders" && <span>{reason}</span>}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className={`btn btn-primary`}>
           Verstuur
         </button>
       </form>
+      {warning}
     </div>
   );
 }
