cb2f995f086d4357460c1555f463f7251c4630d4
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-16 15:21:52 +0100

    werkt, jippie

--- nyala/src/components/ToevoegVeld.jsx
+++ nyala/src/components/ToevoegVeld.jsx
@@ -3,43 +3,50 @@ import styles from "./ToevoegVeld.module.css";
 import AbonnementenFormulier from "./categorieeënComponent";
 
 export default function ToevoegVeld(props) {
-  const [data, setData] = useState({ email: undefined, lijst: undefined });
+  const [data, setData] = useState({ email: undefined, lijst: [] });
   const [status, setStatus] = useState(false);
   const [foutmelding, setFoutmelding] = useState("");
   const [succes, setSucces] = useState(false);
+  const [lijsten, setLijsten] = useState([
+    "ICT",
+    "CMD",
+    "Leden",
+    "Nieuwsbrief",
+  ]);
 
   useEffect(() => {
     if (status) {
       const postEmail = async () => {
-        const response = await fetch(`http://localhost:3001/subscribers/add`, {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            lijst: lijst,
-          }),
-        });
-        if (response.ok) {
-          setData({ email: undefined, lijst: undefined });
-          setStatus(false);
-          setSucces(true);
-        } else if (!response.ok) {
-          setSucces(false);
-        }
+        // const response = await fetch(`http://localhost:3001/subscribers/add`, {
+        //   method: "POST",
+        //   headers: {
+        //     Accept: "application/json",
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     email: data.email,
+        //     lijst: data.lijst,
+        //   }),
+        // });
+        // if (response.ok) {
+        //   setData({ email: undefined, lijst: undefined });
+        //   setStatus(false);
+        //   setSucces(true);
+        // } else if (!response.ok) {
+        //   setSucces(false);
+        // }
+        console.log(data);
       };
-      // postEmail();
+      postEmail();
     }
   }, [status]);
 
-  const melding = 
-    !succes ? ( 
-      <></>
-    ) : (
-    <div className="alert alert-succes" role="alert">
+  const melding = !succes ? (
+    <></>
+  ) : (
+    <div className="alert alert-success d-flex justify-content-around" role="alert">
       <p>Email succesvol toegevoegd.</p>
+      <i class="bi bi-check"></i>
     </div>
   );
 
@@ -47,7 +54,10 @@ export default function ToevoegVeld(props) {
     foutmelding === "" ? (
       <></>
     ) : (
-      <div className="alert alert-danger" role="alert">
+      <div
+        className="alert alert-danger d-flex justify-content-around"
+        role="alert"
+      >
         <p>{foutmelding}</p>
         <i className="bi bi-exclamation-triangle"></i>
       </div>
@@ -55,17 +65,26 @@ export default function ToevoegVeld(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(data);
+
+    let inLijsten = true;
+
+    if (data.lijst.length === 0) {
+      inLijsten = false;
+    } else {
+      data.lijst.forEach((lijst) => {
+        if (!lijsten.includes(lijst)) {
+          inLijsten = false;
+        }
+      });
+    }
+
     if (data.email === "") {
       setFoutmelding("Het emailadres is niet ingevuld.");
-    } else if (data.lijst === "") {
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
+      setFoutmelding("Het emailadres is geen valide formaat!");
+    } else if (data.lijst.length === 0) {
       setFoutmelding("De mailinglijst is niet ingevuld.");
-    } else if (
-      data.lijst !== "Leden" &&
-      data.lijst !== "Nieuwsbrief" &&
-      data.lijst !== "ICT" &&
-      data.lijst !== "CMD"
-    ) {
+    } else if (inLijsten === false) {
       setFoutmelding("De gekozen mailinglijst bestaat niet.");
     } else {
       setSucces(true);
@@ -77,8 +96,19 @@ export default function ToevoegVeld(props) {
     setData({ ...data, email: event.target.value });
   };
 
-  const handleLijstChange = (event) => {
-    setData({ ...data, lijst: event.target.value });
+  const handleCheckboxChange = (event) => {
+    const listName = event.target.value;
+    console.log(listName);
+    const isSelected = data.lijst.includes(listName);
+
+    if (isSelected) {
+      setData({
+        ...data,
+        lijst: [...data.lijst.filter((list) => list !== listName)],
+      });
+    } else {
+      setData({ ...data, lijst: [...data.lijst, listName] });
+    }
   };
 
   return (
@@ -101,20 +131,11 @@ export default function ToevoegVeld(props) {
               aria-describedby="basic-addon1"
               onChange={handleEmailChange}
             />
-            <div className={`${styles.selectContainer}`}>
-              <select
-                className={`form-select`}
-                aria-label="Mailinglijst"
-                onChange={handleLijstChange}
-              >
-                <option value="niet gekozen">Kies een mailinglijst</option>
-                <option value="Leden">Leden</option>
-                <option value="Nieuwsbrief">Nieuwsbrief</option>
-                <option value="ICT">ICT</option>
-                <option value="CMD">CMD</option>
-              </select>
-            </div>
-            {AbonnementenFormulier()}
+              <AbonnementenFormulier
+                abonnees={lijsten}
+                setValue={handleCheckboxChange}
+              />
+            <div className={`${styles.selectContainer}`}></div>
           </div>
           <input
             type="submit"
@@ -123,6 +144,9 @@ export default function ToevoegVeld(props) {
           />
         </form>
       </div>
+      {data.lijst.map((lijst) => {
+        <p>{lijst}</p>;
+      })}
     </div>
   );
 }
