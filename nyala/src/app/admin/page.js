77cb1d01cb45f33c633733539f1209c08eecafdc
julesk1702 <89418218+julesk1702@users.noreply.github.com>
2023-11-27 12:44:18 +0100

    added filter by title

--- nyala/src/app/admin/page.js
+++ nyala/src/app/admin/page.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import FilterPanel from "@/components/adminpanel/FilterComponent";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
 import CardList from "@/components/adminpanel/CardListComponent";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -13,6 +15,8 @@ import { nanoid } from "nanoid";
 function Page() {
   const [templates, setTemplates] = useState({});
   const [error, setError] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +34,7 @@ function Page() {
           };
         });
         setTemplates(templateObject);
+        setIsLoading(false);
       } catch (error) {
         setError(true);
       }
@@ -42,25 +47,98 @@ function Page() {
     return nanoid(); // Generates a unique short ID
   }
 
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+  const filteredTemplates = Object.values(templates).filter((template) =>
+    template.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   const navigateToEditor = () => {
     const newTemplateId = generateUniqueShortId();
-    console.log(newTemplateId);
     window.location.href = `/admin/mail/${newTemplateId}`;
   };
 
   return (
     <Container>
       <Row>
+        <Col md={4}>
+          <Card style={{ height: "fit-content" }}>
+            <Card.Body className="p-4">
+              <Card.Title>Filter</Card.Title>
+              <Form>
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <FormGroup>
+                      <Form.Label>Titel</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Vul hier de titel in"
+                        value={searchValue}
+                        onChange={handleSearchChange}
+                      />
+                    </FormGroup>
+                  </div>
+                  {/* <div className="col-md-6 mb-3">
+                    <FormGroup>
+                      <Form.Label>Categorie</Form.Label>
+                      <Form.Select>
+                        <option>Alle categorieën</option>
+                        <option>Nieuwsbrieven</option>
+                        <option>CMD</option>
+                        <option>ICT</option>
+                      </Form.Select>
+                    </FormGroup>
+                  </div> */}
+                </div>
+
+                {/* <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <FormGroup>
+                      <Form.Label>
+                        Laatst gewijzigd
+                        <Form.Check
+                          type="checkbox"
+                          label="Sorteer op datum"
+                        />
+                      </Form.Label>
+                    </FormGroup>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <FormGroup>
+                      <Button
+                        variant="primary"
+                        className={`${styles.knopPrimary}`}
+                        size="sm"
+                      >
+                        Filter
+                      </Button>
+                    </FormGroup>
+                  </div>
+                </div> */}
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
         <Col md={8}>
           {error && (
             <Alert key="danger" variant="danger">
               Er is een fout opgetreden bij het ophalen van de templates...
             </Alert>
           )}
-          <Col md={{span: 11, offset: 11}} className="text-right mb-3">
-            <Button variant="primary" className={styles.knopPrimary} onClick={navigateToEditor}><i className="bi bi-plus">Nieuwe template</i></Button>
+          <Col md={{ span: 11, offset: 11 }} className="text-right mb-3">
+            <Button
+              variant="primary"
+              className={styles.knopPrimary}
+              onClick={navigateToEditor}
+            >
+              <i className="bi bi-plus">Nieuwe template</i>
+            </Button>
           </Col>
-          <CardList templates={templates} />
+          {!isLoading && <CardList templates={filteredTemplates} />}
         </Col>
       </Row>
     </Container>
