818e0d70b10b2ca73a3421c90af6d4dea75b10f7
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-11-28 14:37:46 +0100

    Melding van kopieren

--- nyala/src/components/subscribe/SubscribeLinkButton.js
+++ nyala/src/components/subscribe/SubscribeLinkButton.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./SubscribeLinkButton.module.css";
+import { Modal } from "react-bootstrap";
 
 const protocol = `http`; // e.g. https
 const domain = `localhost:3000`; // e.g. svxtend.nl
@@ -9,18 +10,33 @@ export default function SubscribeLinkButton(props) {
   const list = props.list;
   const link = `${protocol}://${domain}/${list}/subscribe`;
 
+  const [showNotification, setShowNotification] = useState(false);
+
   const handleCopy = () => {
     navigator.clipboard.writeText(link);
+    setShowNotification(true);
+
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 1000);
   };
+
+  const handleClose = () => setShowNotification(false);
 
   return (
     <div className={`${styles.around} input-group-prepend`}>
-      <input
-        type="submit"
+      <button
+        type="button"
         className={`btn ${styles.buttonPrimary} rounded p-2`}
-        value="Link naar de lijst kopiëren"
+        data-toggle="modal"
+        data-target="#exampleModal"
         onClick={handleCopy}
-      />
+      >
+        Link naar de lijst kopiëren
+      </button>
+      <Modal show={showNotification} onHide={handleClose}>
+        <Modal.Body>Link gekopieerd!</Modal.Body>
+      </Modal>
     </div>
   );
 }
