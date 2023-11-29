a98967230deb6c8e1403393b69782858ddf3a5a7
Matthias Budding <MD.Budding@student.han.nl>
2023-11-29 14:09:32 +0100

    Update SubscribersTable.jsx

--- nyala/src/components/views/SubscribersTable.jsx
+++ nyala/src/components/views/SubscribersTable.jsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import AlertComponent from "../alert/AlertComponent";
+import styles from "./Views.module.css";
 
 export default function SubscribersTable() {
   const [subscribers, setSubscribers] = useState([]);
+  const [notification, setNotification] = useState({
+    type: "",
+    message: "",
+  });
 
   useEffect(() => {
     fetch("http://localhost:3001/subscribers/all")
@@ -14,6 +20,9 @@ export default function SubscribersTable() {
 
   return (
     <div>
+      <AlertComponent notification={notification} />
+
+      <h1 className="text-center mb-2">Zie hier alle geabonneerde</h1>
       <div className="table-responsive p-5">
         <table className="table table-hover">
           <caption>Lijst met alle geabonneerde leden</caption>
@@ -22,7 +31,8 @@ export default function SubscribersTable() {
               <th scope="col">Naam</th>
               <th scope="col">Email</th>
               <th scope="col">Abonnementen</th>
-              <th> </th>
+              <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody class="table-group-divider">
@@ -33,7 +43,13 @@ export default function SubscribersTable() {
                 <td>{subscriber.subscription.join(", ")}</td>
                 <td className="hover-icon">
                   <i
-                    className="bi bi-trash-fill"
+                    className={`bi bi-pencil-fill ${styles.icon}`}
+                    onClick={() => handleShow(subscriber.email, mailList)}
+                  ></i>
+                </td>
+                <td className="hover-icon">
+                  <i
+                    className={`bi bi-trash-fill ${styles.icon}`}
                     onClick={() => handleShow(subscriber.email, mailList)}
                   ></i>
                 </td>
