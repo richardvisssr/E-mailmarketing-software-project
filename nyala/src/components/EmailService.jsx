0d34c65502c42626006b6d1613aaa50ab1dc0e96
Richard Visser <HJ.Visser@student.han.nl>
2023-12-04 12:48:17 +0100

    push

---
+++ nyala/src/components/EmailService.jsx
@@ -0,0 +1,28 @@
+async function sendDataToSendEmail(html, subscribers, subject, showHeader, id) {
+  console.log("sendDataToSendEmail");
+  try {
+    const response = await fetch("http://localhost:3001/sendEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        html,
+        subscribers,
+        subject,
+        showHeader,
+        id,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+export default sendDataToSendEmail;
--- nyala/src/components/email/SendMail.jsx
+++ nyala/src/components/email/SendMail.jsx
@@ -1,22 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import SubscriptionForm from "../categories/CategoriesComponent";
-import { Alert } from 'react-bootstrap';
 
-function SelectMailingLists({ id }) {
+function SelectMailingLists(props) {
+  const { id } = props;
   const [mailingList, setMailingLists] = useState([]);
   const [selectedMailingList, setSelectedMailingList] = useState([]);
   const [subscribers, setSubscribers] = useState([]);
   const [html, setHtml] = useState("");
-  const [emailSent, setEmailSent] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+
 
   useEffect(() => {
     fetch("http://localhost:3001/mail/getList")
       .then((response) => response.json())
       .then((data) => setMailingLists(data))
-      .catch((error) => setShowError(true) && setErrorMessage(error));
+      .catch((error) => alert(error));      
   }, []);
 
   useEffect(() => {
@@ -42,6 +40,7 @@ function SelectMailingLists({ id }) {
         .then(([subscribersData, emailData]) => {
           setSubscribers(subscribersData);
           setHtml(emailData.html);
+          props.onDataChange({ html, subscribersData });
         })
         .catch((error) => console.error("Error in Promise.all:", error));
     }
@@ -56,75 +55,17 @@ function SelectMailingLists({ id }) {
         return prevSelected.filter((item) => item !== value);
       }
     });
-  };
-
-  const handleSendEmailClick = async () => {
-    if (selectedMailingList.length > 0 && subscribers.length > 0) {
-      try {
-        const response = await fetch(
-          "http://localhost:3001/sendMail/sendEmail",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              html: html,
-              subscribers: subscribers,
-              id: id,
-            }),
-          }
-        );
-
-        const secondResponse = await fetch(
-          "http://localhost:3001/mail/sendEmail",
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              html: html,
-              id: id,
-              subscribers: subscribers,
-            }),
-          }
-        );
-
-        if (!response.ok || !secondResponse.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        setEmailSent(true);
-      } catch (error) {
-        setErrorMessage(`Error sending email: ${error}`);
-        setShowError(true);
-        setEmailSent(false);
+    props.setEmails((prevSelected) => {
+      if (checked) {
+        return [...prevSelected, value];
+      } else {
+        return prevSelected.filter((item) => item !== value);
       }
-    } else {
-      setErrorMessage("Selecteer een mailinglijst met subscribers.");
-      setShowError(true);
-    }
+    });
   };
 
   return (
     <div className="container mt-4">
-      {showError && (
-        <Alert variant="danger" onClose={() => setShowError(false)} dismissible>
-          {errorMessage}
-        </Alert>
-      )}
-      {emailSent && (
-        <div className="alert alert-success" role="alert">
-          E-mail is succesvol verstuurd!
-        </div>
-      )}
-      {subscribers.length === 0 && selectedMailingList.length > 0 && (
-        <div className="alert alert-warning" role="alert">
-          Er zijn nog geen subscribers.
-        </div>
-      )}
-
       <label className="form-label">Selecteer Mailinglijst</label>
       <SubscriptionForm
         subscribers={mailingList[0]?.mailList || []}
@@ -150,10 +91,6 @@ function SelectMailingLists({ id }) {
               ))}
             </tbody>
           </table>
-
-          <button className="btn btn-primary" onClick={handleSendEmailClick}>
-            Mail Versturen
-          </button>
         </div>
       )}
     </div>
