4a0c037bd7f910b391628c816e0b6af3c1ae7a66
Matthias Budding <MD.Budding@student.han.nl>
2023-12-13 11:17:46 +0100

    Updates

--- nyala/src/components/analyse/analysePanel.jsx
+++ nyala/src/components/analyse/analysePanel.jsx
@@ -5,6 +5,7 @@ import * as d3 from "d3";
 import AlertComponent from "../alert/AlertComponent";
 import reasonChart from "./reasonChart";
 import styles from "./analyse.module.css";
+import { Container } from "react-bootstrap";
 
 export default function AnalysePanel({ props }) {
   const chartRef = useRef(null);
@@ -46,66 +47,79 @@ export default function AnalysePanel({ props }) {
   };
 
   useEffect(() => {
+    getUnsubscribeReasons();
     if (ready) {
-      const svg = d3.select(chartRef.current);
-
-      const customData = reasonData.map((item) => item.count);
-      const reasons = reasonData.map((item) => item.reason);
-
-      const barHeight = 20;
-      const width = 300;
-      const margin = { top: 40, right: 40, bottom: 60, left: 160 };
-      const height =
-        Math.ceil((customData.length + 0.1) * barHeight) +
-        margin.top +
-        margin.bottom;
-
-      const x = d3
-        .scaleLinear()
-        .domain([0, d3.max(reasonData, (d) => d.count)])
-        .range([margin.left, width - margin.right]);
-
-      const y = d3
-        .scaleBand()
-        .domain(d3.sort(reasonData, (d) => -d.count).map((d) => d.reason))
-        .rangeRound([margin.top, height - margin.bottom])
-        .padding(0.1);
-
-      svg
-        .selectAll("rect")
-        .data(customData)
-        .enter()
-        .append("rect")
-        .attr("x", margin.left)
-        .attr("y", (d, i) => y(reasons[i]))
-        .attr("width", (d) => x(d) - margin.left)
-        .attr("height", y.bandwidth)
-        .attr("fill", "green");
-      svg
-        .append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y));
-
-      svg
-        .append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).ticks(d3.max(customData) / 1));
+      showChart();
     }
   }, [ready]);
 
+  const showChart = () => {
+    const svg = d3.select(chartRef.current);
+
+    const customData = reasonData.map((item) => item.count);
+    const reasons = reasonData.map((item) => item.reason);
+
+    const barHeight = 20;
+    const width = 300;
+    const margin = { top: 40, right: 40, bottom: 60, left: 160 };
+    const height =
+      Math.ceil((customData.length + 0.1) * barHeight) +
+      margin.top +
+      margin.bottom;
+
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(reasonData, (d) => d.count)])
+      .range([margin.left, width - margin.right]);
+
+    const y = d3
+      .scaleBand()
+      .domain(d3.sort(reasonData, (d) => -d.count).map((d) => d.reason))
+      .rangeRound([margin.top, height - margin.bottom])
+      .padding(0.2);
+
+    svg
+      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("width", width)
+      .attr("height", height);
+
+    svg
+      .selectAll("rect")
+      .data(customData)
+      .enter()
+      .append("rect")
+      .attr("x", margin.left)
+      .attr("y", (d, i) => y(reasons[i]))
+      .attr("width", (d) => x(d) - margin.left)
+      .attr("height", y.bandwidth)
+      .attr("fill", "#E800E9");
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(d3.max(customData) / 1));
+
+    svg
+      .append("text")
+      .attr("x", width / 2)
+      .attr("y", margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .text("Aantal afmeldredenen");
+  };
+
   return (
     <div>
-      {" "}
       <AlertComponent notification={notification} />
-      <div>
-        {" "}
-        <button onClick={getUnsubscribeReasons}>Get unsubscribe reasons</button>
-      </div>{" "}
-      <div>
-        {" "}
-        <div className={`d-flex justify-content-center w-50`}>
-          <svg ref={chartRef} />
-        </div>
+      <div
+        className={`container-lg ${styles.quarterPageContainer} position-fixed top-30`}
+      >
+        <svg className={`${styles.chart}`} ref={chartRef} />
       </div>
     </div>
   );
--- nyala/src/components/analyse/reasonChart.jsx
+++ nyala/src/components/analyse/reasonChart.jsx
@@ -1,50 +1,63 @@
-"use client";
-
+// ReasonChart.js
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-export default function reasonChart(data, ref) {
+const ReasonChart = ({ data }) => {
   const chartRef = useRef(null);
 
   useEffect(() => {
     const svg = d3.select(chartRef.current);
 
-    const width = 500;
-    const height = 300;
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const customData = data.map((item) => item.count);
+    const reasons = data.map((item) => item.reason);
 
-    const xScale = d3
+    const barHeight = 20;
+    const width = 300;
+    const margin = { top: 40, right: 40, bottom: 60, left: 160 };
+    const height =
+      Math.ceil((customData.length + 0.1) * barHeight) +
+      margin.top +
+      margin.bottom;
+
+    const x = d3
       .scaleLinear()
-      .domain([0, 100])
+      .domain([0, d3.max(data, (d) => d.count)])
       .range([margin.left, width - margin.right]);
 
-    const yScale = d3
+    const y = d3
       .scaleBand()
-      .domain(["Reason 1", "Reason 2", "Reason 3"])
-      .range([margin.top, height - margin.bottom])
+      .domain(d3.sort(data, (d) => -d.count).map((d) => d.reason))
+      .rangeRound([margin.top, height - margin.bottom])
       .padding(0.1);
 
     svg
+      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("width", width)
+      .attr("height", height);
+
+    svg
       .selectAll("rect")
-      .data(data)
+      .data(customData)
       .enter()
       .append("rect")
-      .attr("x", xScale(0))
-      .attr("y", (d) => yScale(d))
-      .attr("width", (d) => xScale(d))
-      .attr("height", yScale.bandwidth())
-      .attr("fill", "steelblue");
+      .attr("x", margin.left)
+      .attr("y", (d, i) => y(reasons[i]))
+      .attr("width", (d) => x(d) - margin.left)
+      .attr("height", y.bandwidth)
+      .attr("fill", "green");
 
     svg
       .append("g")
       .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(yScale));
+      .call(d3.axisLeft(y));
 
     svg
       .append("g")
       .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
-  }, []);
+      .call(d3.axisBottom(x).ticks(d3.max(customData) / 1));
+  }, [data]);
 
-  return <svg ref={chartRef} />;
-}
+  return <svg viewBox="0 0 60 55" width="200" height="100" ref={chartRef} />;
+};
+
+export default ReasonChart;
