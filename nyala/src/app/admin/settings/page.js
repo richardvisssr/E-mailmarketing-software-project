801f5539d08392a3d7be9b769ad7999ad541b9dd
julesk1702 <89418218+julesk1702@users.noreply.github.com>
2023-12-19 10:58:25 +0100

    commit

--- nyala/src/app/admin/settings/page.js
+++ nyala/src/app/admin/settings/page.js
@@ -1,24 +1,8 @@
 "use client";
 import SettingsComponent from "@/components/settingsComponent/SettingsComponent";
-import React, { useState } from "react";
+import React from "react";
 
 const SettingsPage = () => {
-  const [interval, setInterval] = useState("");
-  const [token, setToken] = useState("");
-
-  const handleIntervalChange = (e) => {
-    setInterval(e.target.value);
-  };
-
-  const handleTokenChange = (e) => {
-    setToken(e.target.value);
-  };
-
-  const copyTokenToClipboard = () => {
-    navigator.clipboard.writeText(token);
-    alert("Token copied to clipboard");
-  };
-
   return (
     <SettingsComponent />
   );
