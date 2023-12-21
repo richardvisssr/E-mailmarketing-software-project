9389d8af51270a27babc0e146397aca626ad943c
MDA-Han-Student <117663171+MDA-Han-Student@users.noreply.github.com>
2023-12-21 15:55:47 +0100

    not-found works

--- nyala/src/app/not-found.js
+++ nyala/src/app/not-found.js
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export default function page() {
+export default function NotFound() {
   const router = useRouter();
 
   useEffect(() => {
