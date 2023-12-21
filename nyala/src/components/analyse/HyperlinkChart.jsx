d34c679abe7d0183ef4d4093f098288f30886a9d
Matthias Budding <MD.Budding@student.han.nl>
2023-12-21 09:17:32 +0100

    Update

--- nyala/src/components/analyse/HyperlinkChart.jsx
+++ nyala/src/components/analyse/HyperlinkChart.jsx
@@ -79,7 +79,7 @@ const UnsubscribeReasonChart = ({ linkData }) => {
       .style("font-size", "16px")
       .text("Aantal keren dat er op een link is geklikt");
   };
-  return <svg className={`${styles.chart}`} ref={chartRef} />;
+  return <svg className={`${styles.barChart}`} ref={chartRef} />;
 };
 
 export default UnsubscribeReasonChart;
