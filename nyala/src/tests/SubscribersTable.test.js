4eeaad2d2a6aad1c5dc57b5832b63d4ea89ed580
Matthias Budding <MD.Budding@student.han.nl>
2024-01-10 11:46:11 +0100

    Update

---
+++ nyala/src/tests/SubscribersTable.test.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useRouter } from "next/navigation";
+import { createRoot } from "react-dom/client";
+
+import SubscribersTable from "../components/views/SubscribersTable";
+
+jest.mock("next/navigation");
+
+jest.mock("js-cookie", () => ({
+  get: jest.fn(() => "mockedToken"),
+}));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+);
+
+describe("SubscribersTable", () => {
+  it("should render the subscribers table component with no subscribers", async () => {
+    const div = document.createElement("div");
+    const root = createRoot(div);
+
+    await act(async () => {
+      useRouter.mockImplementation(() => ({
+        pathname: "",
+        route: "",
+        query: "",
+        prefetch: jest.fn(),
+      }));
+
+      root.render(<SubscribersTable />);
+    });
+
+    expect(root.container).toBeDefined();
+    expect(root.container).toMatchSnapshot();
+    console.log(root.container);
+  });
+
+  it("should render the subscribers table component with subscribers", async () => {
+    const div = document.createElement("div");
+    const root = createRoot(div);
+
+    await act(async () => {
+      useRouter.mockImplementation(() => ({
+        pathname: "",
+        route: "",
+        query: "",
+        prefetch: jest.fn(),
+      }));
+
+      const mockSubscribers = [
+        {
+          name: "John Doe",
+          email: "john@example.com",
+          subscription: ["A", "B"],
+        },
+        {
+          name: "Jane Smith",
+          email: "jane@example.com",
+          subscription: ["C", "D"],
+        },
+      ];
+
+      global.fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          json: () => Promise.resolve(mockSubscribers),
+        })
+      );
+
+      root.render(<SubscribersTable />);
+    });
+
+    expect(root.container).toMatchSnapshot();
+  });
+});
